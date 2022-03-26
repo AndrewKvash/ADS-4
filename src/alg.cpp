3ce2c809ed762f648e861d10856287cf1d1df282
AndrewKvash <94719715+AndrewKvash@users.noreply.github.com>
2022-03-26 21:08:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
 int res = 0;
-for (int i = 0; i < len;i++) {
+for (int i = 0; i < len; i++) {
 for (int j = i + 1; j < len; j++) {
 if (arr[i] + arr[j] == value) {
 res++;
@@ -31,13 +31,13 @@ while (l < r) {
 int ser = (l + r) / 2;
 if (arr[ser] < (value - arr[i])) {
 l = ser + 1;
-}else {
+} else {
 r = ser;
 }
 }
 while (arr[l] + arr[i] == value) {
 res++;
-l ++;
+l++;
 }
 }
 return res;
